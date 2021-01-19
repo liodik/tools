91b4c416be636f4883ee5020ea27d6a4d9c1d67a
Volodymyr Meduna <liodik89@gmail.com>
2021-01-19 16:06:42 +0200

    commit

---
+++ lesson9/task1/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: 'eslint-config-airbnb-base',
+
+  rules: {
+    // 'no-console': 2,
+    // eqeqeq: 2,
+  },
+  parserOptions: {
+    ecmaScript: 10,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+  },
+};
